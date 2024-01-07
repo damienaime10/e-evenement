fe96e91771c25c172e292a7fcc0eb31cd9037e53
aime damien amandete <a12damien@kali.aimedamien.com>
2024-01-07 13:32:23 +0100

    first commit Damien

---
+++ src/main/java/com/example/model/ModelEvenementApplication.java
@@ -0,0 +1,10 @@
+package com.example.model;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
